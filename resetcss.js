c658f5bdc0ac11f857f1b6781d975c3bc67ff920
guanyx <476848761@163.com>
2016-08-24 00:13:16 +0800

    update

--- resetcss.js
+++ resetcss.js
@@ -1,3 +1,47 @@
 let style = document.createElement('style');
-style.innerHTML = ``;
+style.innerHTML = `html,
+body{
+    height: 100% !important;
+}
+
+.x-grid-panel{
+    width: 100% !important;
+}
+
+.x-panel-body{
+    width: 100% !important;
+}
+
+.x-grid3-scroller{
+    width: 100% !important;
+}
+
+.x-grid3-body{
+    width: 100% !important;
+}
+
+.x-grid3-row{
+    width: 100% !important;
+}
+
+.x-grid3-row-table{
+    width: 100% !important;
+}
+
+.x-grid3-cell{
+    height: 40px !important;
+    vertical-align: middle !important;
+}
+
+.x-grid3-col-checker{
+    height: auto !important;
+}
+
+td.x-grid3-hd-over .x-grid3-hd-inner,
+td.sort-desc .x-grid3-hd-inner,
+td.sort-asc .x-grid3-hd-inner,
+td.x-grid3-hd-menu-open .x-grid3-hd-inner{
+    background-color: transparent !important;
+    background-image: none !important;
+}`;
 document.head.appendChild(style);
